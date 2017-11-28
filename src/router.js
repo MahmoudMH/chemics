260c04d8954d45a24f1d37d26fd06e1260477ae1
samar <sasa-2010-1995@hotmail.com>
2017-11-28 21:22:03 +0200

    d

--- src/router.js
+++ src/router.js
@@ -2,17 +2,39 @@ const fs = require('fs');
 const path = require('path');
 const router = (request , response) => {
   var url = request.url;
-  console.log('here',url);
   if( url === '/alchemy'){
-    var allTheData = '';
+    var input = '';
+    var nameElm = [];
   request.on('data', function (chunkOfData) {
     var textChunk = chunkOfData.toString('utf8');
-
-      allTheData += chunkOfData;
-      console.log(typeof textChunk);
+      input += textChunk;
   });
 
-  request.on('end', function () { });
+  request.on('end', function () {
+    fs.readFile(path.join(__dirname,'data','data.json'),(error, file)=>{
+          if(error){
+            response.writeHead(500,'content-Type: text/html');
+            response.end('<h1>internal server Error</h1>');
+          }
+          var elements = JSON.parse(JSON.stringify(file));
+          // for(var i in elements[0]){nameElm.push(elements[0][i].name);}
+          // for (var i = 0; i < elements[0].length; i++) {
+          //   nameElm.push(elements[0][i].name);
+          // }
+          console.log('sssd',elements.elements[1].name);
+
+            // const elements = JSON.parse(JSON.stringify(file)).elements;
+            // var ret = [];
+            // for(var i=0; i<elements.length; i++) {
+            // if(elements[i].name.toLowerCase().indexOf(input.toLowerCase()) !== -1) {
+            // ret.push(elements[i]);
+            // }
+            // }
+            console.log('sssss',JSON.parse(file));
+            response.end();
+
+        });
+  });
   response.end();
 }
 
