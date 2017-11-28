2689ade8d00432b5a218fc44e0b4203ca8343983
samar <sasa-2010-1995@hotmail.com>
2017-11-28 15:39:42 +0200

    modified stuff

--- public/script.js
+++ public/script.js
@@ -1,1 +1,13 @@
-
+var input = getElementById('input');
+input.addEventListener('keyup', function() {
+  var name = input.value;
+  var xhr = new XMLHttpRequest();
+    xhr.SetRequestHeade("Conect-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+  if(xhr.readyState == 4 && xhr.status == 200) {
+    var response = xhr.responseText;
+  }
+}
+    xhr.open("POST", "/alchemy");
+    xhr.send(name);
+});
