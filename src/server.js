03335b57a6d02f9b09e68132bf1e0aa3f6c0f612
Yahya Albarrawi <yalbarrawoui256@gmail.com>
2017-11-29 17:25:33 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const router = require('./router');
 const server = http.createServer(router);
 
-server.listen (process.env.port || 4000, result =>{
+server.listen (process.env.PORT || 4000, result =>{
   console.log("passed");
 });
