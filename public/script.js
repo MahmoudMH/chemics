ea5c16ef5427557b1b2ef863befcd496ea3cd6d5
samar <sasa-2010-1995@hotmail.com>
2017-11-29 14:59:53 +0200

    bonus work

--- public/script.js
+++ public/script.js
@@ -10,20 +10,37 @@ input.addEventListener('keyup', function() {
       response = JSON.parse(xhr.responseText);
       console.log('response: ', response);
       div.innerHTML = '';
+      let anchor = document.createElement('a');
       for (var i in response) {
-        let anchor = document.createElement('a');
         anchor.innerHTML = response[i].name + ", " + response[i].atomic_mass;
-        anchor.setAttribute('href',response[i].source);
-        anchor.setAttribute('target','_blank');
-        // anchor.setAttribute('ac')
+        anchor.setAttribute('href', response[i].source);
+        anchor.setAttribute('target', '_blank');
         div.appendChild(anchor);
       }
+      anchor.addEventListener('mouseover', function() {
+        let elmentObject = response[i];
+        console.log('Hover');
+        var div = document.createElement('div');
+        div.setAttribute('class', 'comment-box');
+        var p = document.createElement('p');
+        p.innerHTML = 'Elment name: ' + elmentObject.name + '<br>' +
+        'Appearance: ' + elmentObject.appearance + '<br>' +
+        'Atomic Mass: ' + elmentObject.atomic_mass + '<br>' +
+        'Boil Temprature: ' + elmentObject.boil + '<br>' +
+        'Catagory: ' + elmentObject.Catagory + '<br>' +
+        'Color: ' + elmentObject.color + '<br>';
+        div.appendChild(p);
+        document.getElementsByTagName('body')[0].appendChild(div);
+      });
+      anchor.addEventListener('mouseout', function() {
+        document.getElementsByClassName('comment-box').innerHTML = '';
+      });
       div.style.visibility = 'visible';
-      if(input.value=='') {
+      if (input.value == '') {
         div.innerHTML = '';
         div.style.visibility = 'hidden';
       }
-      console.log(input.value=='');
+
     }
   }
   xhr.open("POST", "/chemics");
