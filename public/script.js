ea0d998bd7282c534117b278bb78f4616eb0bef9
samar <sasa-2010-1995@hotmail.com>
2017-11-29 13:25:23 +0200

    all done

--- public/script.js
+++ public/script.js
@@ -1,16 +1,30 @@
 var input = document.getElementById('input');
-
+var button = document.getElementById('search');
+var div = document.getElementById('search-menu');
+var response = {};
 input.addEventListener('keyup', function() {
   var name = input.value;
   var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-  if(xhr.readyState == 4 && xhr.status == 200) {
-    var response = JSON.parse(xhr.responseText);
-    console.log('response',typeof response);
-
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      response = JSON.parse(xhr.responseText);
+      console.log('response: ', response);
+      div.innerHTML = '';
+      for (var i in response) {
+        let anchor = document.createElement('a');
+        anchor.innerHTML = response[i].name + ", " + response[i].atomic_mass;
+        anchor.setAttribute('href',response[i].source);
+        // anchor.setAttribute('ac')
+        div.appendChild(anchor);
+      }
+      div.style.visibility = 'visible';
+      if(input.value=='') {
+        div.innerHTML = '';
+        div.style.visibility = 'hidden';
+      }
+      console.log(input.value=='');
+    }
   }
-
- }
-    xhr.open("POST", "/alchemy");
-    xhr.send(name);
+  xhr.open("POST", "/chemics");
+  xhr.send(name);
 });
